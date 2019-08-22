174d6f04696ffb9f44c68e451c49da5e19c1e767
SKY IT <SKY IT@DESKTOP-S4R1PH8>
2019-08-22 13:24:56 +0600

    this is second commit

--- src/main/java/com/dipu/primaryaccounting/model/AccountChart.java
+++ src/main/java/com/dipu/primaryaccounting/model/AccountChart.java
@@ -33,7 +33,7 @@ public class AccountChart implements Serializable {
     @Column
     private String root;
     @Column
-    private String status;
+    private String status="true";
 
     public int getAc_id() {
         return ac_id;
